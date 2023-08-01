19162fa4d2c587bebacbd79156ec5a84c23d9a9d
Tony Hunter <thunter1987@yahoo.com>
2023-08-01 18:10:59 -0500

    Passes most Teacher Tests

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.knowledge = []
+        
+    def learn(self, string):
+        self.knowledge.append(string)
+                
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,25 @@ import random
 
 class Teacher(User):
 
-    def teach(self):
-        pass
+        @classmethod
+        def teach(self):
+            knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",]
+            self.knowledge.append(random.choice( knowledge ))
+            
+        def __init__(self, first_name, last_name, knowledge = None):
+            super().__init__(first_name, last_name)
+
+            if knowledge is None:
+                self.knowledge = []
+                self.teach()
+            else:
+                self.knowledge = knowledge
+        
