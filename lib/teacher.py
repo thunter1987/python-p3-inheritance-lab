a94615e9aac9a7e62b87a23ec5e758854d8a22f2
Tony Hunter <thunter1987@yahoo.com>
2023-08-02 16:17:03 -0500

    Passes tests

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,10 +5,10 @@ from user import User
 import random
 
 class Teacher(User):
-
-        @classmethod
-        def teach(self):
-            knowledge = [
+            
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
             "str is a data type in Python",
             "programming is hard, but it's worth it",
             "JavaScript async web request",
@@ -17,14 +17,7 @@ class Teacher(User):
             "programming computers hacking learning terminal",
             "pipenv install pipenv shell",
             "pytest -x flag to fail fast",]
-            self.knowledge.append(random.choice( knowledge ))
-            
-        def __init__(self, first_name, last_name, knowledge = None):
-            super().__init__(first_name, last_name)
-
-            if knowledge is None:
-                self.knowledge = []
-                self.teach()
-            else:
-                self.knowledge = knowledge
         
+    
+    def teach(self):
+        return self.knowledge[random.randint(0, len(self.knowledge)-1)]
